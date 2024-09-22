5736c3bfd0f9a1caadc2a96d424d3cda955d60e1
Shoutcape <kautiainen.ville@gmail.com>
2024-09-22 20:20:01 +0300

    added playwrigth.config.js

--- models/person.js
+++ models/person.js
@@ -22,7 +22,7 @@ const personSchema = new mongoose.Schema({
   number: {
     type: String,
     validate: {
-      validator: function(v){
+      validator: function(v) {
         return /^\d{2,3}-\d{6,}$/.test(v)
       },
       message: props => `${props.value} is not a valid phone number`
